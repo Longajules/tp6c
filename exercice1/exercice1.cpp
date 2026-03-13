43eab14e9bff7a0844adc8171211a52db73964b4
Longajules <judonite60@gmail.com>
2026-03-13 23:45:56 +0100

    code termine

--- exercice1/exercice1.cpp
+++ exercice1/exercice1.cpp
@@ -27,6 +27,28 @@ int multipledesix(int a) //permet de determiner si un nombre est un multiple de 
 	return result;
 }
 
+int verification(int a)
+{
+	int resTrois = multipledetrois(a);
+	int resSix = multipledesix(a);
+
+	if (resTrois == 0 && resSix == 0)
+	{
+		//si la valeur de retourne est 2 , alors le nombre est un multiple de 3 et de 6
+		return 2;
+	}
+	else if (resTrois == 0 && resSix != 0)
+	{
+		//si la valeur de retourne est 1 , alors le nombre est un multiple de 3 mais pas de 6
+		return 1;
+	}
+	else
+	{
+		//si la valeur de retourne est 0 , alors le nombre n'est pas un multiple de 3 ni de 6
+		return 0;
+	}
+}
+
 int main()
 {
 	int a = 0;
@@ -35,25 +57,31 @@ int main()
 	printf("Entrez un nombre entier :\n ");
 	scanf_s("%d", &a);
 
-	resultat = multipledesix(a);
+	resultat = verification(a);
 	
-	if (resultat == 0 || resultat == 3)
+	if (resultat == 2)
 	{
-		printf("Le nombre %d est un multiple de 6.\n", a);
+		printf("Le nombre %d est un multiple de 3 et de 6.\n", a);
+	}
+	else if (resultat == 1)
+	{
+		printf("Le nombre %d est un multiple de 3 mais pas de 6.\n", a);
 	}
 	else
 	{
-		resultat = multiplededeux(a);
+		printf("Le nombre %d n'est pas un multiple de 3 ni de 6.\n", a);
 	}
 
-	if (resultat == 0)
-	{
-		printf("Le nombre %d est un multiple de 2.\n", a);
-	}
-	else
-	{
-		printf("Le nombre %d n'est pas un multiple de 2 ni un multiple de 3.\n", a);
-	}
+	 resultat = multiplededeux(a);
+
+	 if (resultat == 0)
+	 {
+		 printf("Le nombre %d est pair.\n", a);
+	 }
+	 else
+	 {
+		 printf("Le nombre %d est impair.\n", a);
+	 }
 
 	return 0;
 }
